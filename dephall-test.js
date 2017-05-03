1414e29e8d998d7a546933d389dbf953e0f64f9a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 12:10:39 +0800

    Updated test.

--- dephall-test.js
+++ dephall-test.js
@@ -1,11 +1,12 @@
 "use strict";
 
+const assert = require( "assert" );
 const dephall = require( "./dephall.js" );
 
-console.log( dephall( [ 1, 2, true, "hello" ], STRING, "yeah" ) );
-console.log( dephall( [ 1, 2, 3, true ], STRING, "", "yeah" ) );
-console.log( dephall( [ 1, 2, 3, true, "yeah", "ugh" ], [ STRING, OBJECT ], "", "yeah" ) );
+assert.deepEqual( dephall( [ 1, 2, true, "hello" ], STRING, "yeah" ), [ "hello" ] );
+assert.deepEqual( dephall( [ 1, 2, 3, true ], STRING, "", "yeah" ), [ "yeah" ] );
+assert.deepEqual( dephall( [ 1, 2, 3, true, "yeah", "ugh" ], [ STRING, OBJECT ], "", "yeah" ), [ "yeah", "ugh" ] );
+assert.deepEqual( dephall( [ 1,2,3 ], STRING, "", 123, "yeah" ), [ "yeah" ] );
+assert.deepEqual( dephall( [ 1,2,3 ], Array, "", 123, "yeah", [1,2,3] ), [ [ 1, 2, 3 ] ] );
 
-console.log( dephall( [ 1,2,3 ], STRING, "", 123, "yeah" ) );
-
-console.log( dephall( [ 1,2,3 ], Array, "", 123, "yeah", [1,2,3] ) );
+console.log( "ok" );
