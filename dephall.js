9166587b467683b2a4cd1d55742f9fe6663bb0d8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-19 11:52:41 +0800

    Updated server/client test.

--- dephall.js
+++ dephall.js
@@ -32,10 +32,11 @@
 			"file": "dephall.js",
 			"module": "dephall",
 			"author": "Richeve S. Bebedor",
-			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-			],
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/dephall.git",
 			"global": true,
 			"class": true
